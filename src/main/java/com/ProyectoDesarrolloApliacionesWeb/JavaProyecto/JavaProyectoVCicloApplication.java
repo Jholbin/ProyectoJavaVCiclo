f24bb872f4f1c99e3747044060b4b902c508dd00
Jholbin <jsub7777@gmail.com>
2023-07-24 14:38:50 -0500

    Primera Entrega

---
+++ src/main/java/com/ProyectoDesarrolloApliacionesWeb/JavaProyecto/JavaProyectoVCicloApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoDesarrolloApliacionesWeb.JavaProyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProyectoVCicloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProyectoVCicloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProyectoDesarrolloApliacionesWeb/JavaProyecto/JavaProyectoVCicloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoDesarrolloApliacionesWeb.JavaProyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProyectoVCicloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
